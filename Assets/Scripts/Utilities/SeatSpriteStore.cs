c3f306fb5a133cddd803340f7d86819b0771300f
Yong Jun Xi <89063663+TobyCyan@users.noreply.github.com>
2026-07-25 01:02:19 +0800

    Merge pull request #11 from TobyCyan/mg-window
    Create mini game view and initialize seats with random color

---
+++ Assets/Scripts/Utilities/SeatSpriteStore.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using UnityEngine;
+using static Seat;
+
+public class SeatSpriteStore
+{
+    private const string SPRITE_SHEET_PATH = "Sprites/Environment";
+
+    private static readonly Dictionary<SeatColor, string> COLOR_TABLE_SPRITE_NAME_MAP = new()
+    {
+        { SeatColor.RED, "Table_Red" },
+        { SeatColor.BLUE, "Table_Blue" },
+        { SeatColor.ORANGE, "Table_Orange" },
+        { SeatColor.GREEN, "Table_Green" },
+    };
+
+    private static readonly Dictionary<SeatColor, string> COLOR_STOOL_SPRITE_NAME_MAP = new()
+    {
+        { SeatColor.RED, "Stool_Red" },
+        { SeatColor.BLUE, "Stool_Blue" },
+        { SeatColor.ORANGE, "Stool_Orange" },
+        { SeatColor.GREEN, "Stool_Green" },
+    };
+
+    private static readonly Dictionary<string, Sprite> spritesNameMap = new();
+
+    private static void EnsureSpritesLoaded()
+    {
+        if (spritesNameMap.Count > 0) return;
+
+        var sprites = Resources.LoadAll<Sprite>(SPRITE_SHEET_PATH);
+
+        foreach (var sprite in sprites)
+        {
+            spritesNameMap[sprite.name] = sprite;
+        }
+    }
+
+    public static Sprite GetTableSprite(SeatColor color)
+    {
+        EnsureSpritesLoaded();
+        return spritesNameMap[COLOR_TABLE_SPRITE_NAME_MAP[color]];
+    }
+
+    public static Sprite GetStoolSprite(SeatColor color)
+    {
+        EnsureSpritesLoaded();
+        return spritesNameMap[COLOR_STOOL_SPRITE_NAME_MAP[color]];
+    }
+}
