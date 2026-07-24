f63ea83e3e5a53d586b15883652d3197ffb635aa
YongJunXi <ernestyongjunxi@gmail.com>
2026-07-24 17:13:03 +0800

    Add interface comment

--- Assets/Scripts/Core/Mini Games/IMiniGameMaster.cs
+++ Assets/Scripts/Core/Mini Games/IMiniGameMaster.cs
@@ -1,5 +1,9 @@
 using System;
 
+/// <summary>
+/// Interface for mini-game masters, which are responsible for generating and cleaning up mini-games.
+/// Game masters should decide how is the mini-game considered completed and invoke the event.
+/// </summary>
 interface IMiniGameMaster
 {
     public event Action OnMiniGameCompleted;
