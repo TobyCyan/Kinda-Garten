e1845d3418b3cc3e2d236f95ae256639b8db8e18
YongJunXi <ernestyongjunxi@gmail.com>
2026-07-26 21:03:02 +0800

    Adjust configs

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -14,15 +14,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+        Instance = this;
 
         player = FindAnyObjectByType<PlayerController>();
         InitConfigs();
