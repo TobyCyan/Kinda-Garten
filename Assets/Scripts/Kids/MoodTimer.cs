2274b7736bfc9dbbd8f8b54dc4514a676b678f55
Yong Jun Xi <89063663+TobyCyan@users.noreply.github.com>
2026-07-26 14:36:11 +0800

    Implement mood timer UI color changes (#37)
    * Format mood color
    
    * Fix mood color changing by float
    
    * Adjust mood thresholds

--- Assets/Scripts/Kids/MoodTimer.cs
+++ Assets/Scripts/Kids/MoodTimer.cs
@@ -6,8 +6,15 @@ public class MoodTimer : MonoBehaviour
 {
     [SerializeField] private TextMeshPro moodTimerText;
     [SerializeField] private GameObject moddTimerObject;
+    public float BaseMoodTimer { get; private set; }
 
     public event Action<float> OnTimerUpdate;
+
+    public void Init(float baseMoodTimer)
+    {
+        BaseMoodTimer = baseMoodTimer;
+        RefreshUI(baseMoodTimer);
+    }
 
     public void SetVisiblity(bool isVisible)
     {
@@ -16,7 +23,9 @@ public class MoodTimer : MonoBehaviour
 
     public void RefreshUI(float currentMoodTimer)
     {
-        moodTimerText.text = currentMoodTimer.ToString("F0");
+        int displayedTime = Mathf.Max(0, Mathf.CeilToInt(currentMoodTimer));
+        moodTimerText.text = displayedTime.ToString();
+        moodTimerText.color = MoodFormatter.FormatMoodColor(displayedTime, BaseMoodTimer);
         OnTimerUpdate?.Invoke(currentMoodTimer);
     }
 }
--- Assets/Scripts/UI/MoodTimerUI.cs
+++ Assets/Scripts/UI/MoodTimerUI.cs
@@ -20,7 +20,9 @@ public class MoodTimerUI : MonoBehaviour
 
     private void RefreshUI(float currentMoodTimer)
     {
-        moodTimerText.text = currentMoodTimer.ToString("F0");
+        int displayedTime = Mathf.Max(0, Mathf.CeilToInt(currentMoodTimer));
+        moodTimerText.text = displayedTime.ToString();
+        moodTimerText.color = MoodFormatter.FormatMoodColor(displayedTime, moodTimer.BaseMoodTimer);
     }
 
     public void CleanUp()
